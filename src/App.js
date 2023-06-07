59d9a0a00885bc7d78108941f6f2ace450901f49
Belen544 <perezxochitl465@gmail.com>
2023-06-07 04:23:06 -0600

    first commit

---
+++ src/App.js
@@ -0,0 +1,41 @@
+import logo from './logo.svg';
+import './App.css';
+import Personajes from './Pages/Personajes';
+import Comics from './Pages/Comics';
+import Series from './Pages/Series';
+
+import {BrowserRouter as Router, Routes, Route, Link} from 'react-router-dom';
+
+function App() {
+  return (
+    <>
+      <body>
+        <div className='general'>
+          <Router>
+            <div className='container'>
+              <div>
+                <h1>PANEL DE OPCIONES</h1>
+              </div>
+              <div>
+              <Link to={'/'}><button id={"buttonAddNew2"} >PERSONAJES</button></Link>
+              <Link to={'/Comics'}><button id={"buttonAddNew2"} >COMICS</button></Link>
+              <Link to={'/Series'}><button id={"buttonAddNew2"} >SERIES</button></Link>
+            
+              </div>
+            </div>
+            <br/>
+            <Routes>
+              <Route path='/' element={<Personajes />} />
+              <Route path='/Comics' element={<Comics />} />
+              <Route path='/Series' element={<Series />} />
+            </Routes>
+          </Router>
+        </div>
+      </body>
+    </>
+  );
+}
+    
+ 
+
+export default App;
---
+++ src/Pages/Comics.jsx
@@ -0,0 +1,47 @@
+import React, { useEffect, useState } from "react";
+import { Link, Outlet, Route, Routes } from "react-router-dom";
+
+
+import axios from "axios";
+import 'bootstrap/dist/css/bootstrap.min.css';
+//import './style.css'
+
+export default function Comics() {
+    const baseUrl = 'https://gateway.marvel.com/v1/public'
+    const [data, setData] = useState([]);
+
+    useEffect(() => {
+        axios.get(`${baseUrl}/comics?ts=1&apikey=d781254a480edce1c3a28770fb303244&hash=85e2f7ba75867851a2fb9d7ea4d5764a`)
+            .then(response => {
+                console.log(response.data.data.results)
+                setData(response.data.data.results);
+            })
+            .catch(error => {
+                console.log(error);
+            });
+    }, []);
+
+    return (
+        <>  
+            <div className="container pt-5">
+                <div className={"row d-flex justify-content-center"}>
+                    <div className={"col-2 d-flex justify-content-center py-3 rounded-5"}
+                         style={{backgroundColor: "rgba(38,30,30,0.56)"}}>
+                        <h2>Comics
+                        </h2>
+                    </div>
+                </div>
+                <div className="row text-uppercase mt-5">
+                    {data.map((item) => (
+                        <div className="mb-4 mx-5 p-2 col-3 text-center fw-bold">
+                            <p style={{fontFamily:"Britannic bold",fontSize:"25px"}}>{item.title}</p>
+
+                            
+                            <img src={item.thumbnail.path + "." + item.thumbnail.extension} alt="" style={{ height: "10rem" }} />
+                        </div>
+                    ))}
+                </div>
+            </div>
+        </>
+    );
+}
